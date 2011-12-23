76b9e74941715c541d846ecdc14db80a1db8ed19
Jeremy McAnally <jeremymcanally@gmail.com>
2011-12-23 10:54:10 -0500

    Check for bad TLDs

--- lib/ditchdaddy.rb
+++ lib/ditchdaddy.rb
@@ -2,5 +2,5 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Ditchdaddy
-  VERSION = '0.0.1'
+  VERSION = '0.0.3'
 end
